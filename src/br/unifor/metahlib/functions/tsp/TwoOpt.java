ff0b9d73a5a70d39b9e80011968525be52831153
mlotifjava@gmail.com <mlotifjava@gmail.com@a08dab08-bcae-11de-bb2d-0d8fa6df8a7d>
2009-10-26 14:53:54 +0000

    Adicinado suporte a intâncias de problemas do TSPLib.

---
+++ src/br/unifor/metahlib/functions/tsp/TwoOpt.java
@@ -0,0 +1,36 @@
+package br.unifor.metahlib.functions.tsp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import br.unifor.metahlib.base.NeighbourhoodStructure;
+
+public class TwoOpt implements NeighbourhoodStructure {
+	
+	@Override
+	public List<double[]> getNeighbours(List<double[]> parents) {
+		
+		Random r = new Random();
+		
+		double[] p = parents.get(0);
+		
+		double[] parent = new double[p.length];
+		for(int i = 0; i < p.length; i++){
+			parent[i] = p[i];
+		}
+		
+		int k1 = r.nextInt(parent.length);
+		int k2 = r.nextInt(parent.length);
+		
+		double aux = parent[k1];
+		parent[k1] = parent[k2];
+		parent[k2] = aux;
+		
+		List<double[]> l = new ArrayList<double[]>();
+		l.add(parent);
+		
+		return l;
+	}
+
+}
