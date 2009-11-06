d3bb7a83cffad771ddb559359b688d806840e2c1
paulocmoreno <paulocmoreno@a08dab08-bcae-11de-bb2d-0d8fa6df8a7d>
2009-11-06 15:52:12 +0000

    - Documentada as classes EdgeWeightType e TourCostEvaluator.

--- trunk/src/br/unifor/metahlib/problems/tsp/TourCostEvaluator.java
+++ trunk/src/br/unifor/metahlib/problems/tsp/TourCostEvaluator.java
@@ -3,14 +3,27 @@ package br.unifor.metahlib.problems.tsp;
 import br.unifor.metahlib.base.CostEvaluator;
 import br.unifor.metahlib.base.Solution;
 
+/**
+ * Cost evaluator of TSP problems.
+ */
 public class TourCostEvaluator extends CostEvaluator {
 	
+	/**
+	 * TSP dataSet with cities distances.
+	 */
 	private TSPDataSet dataSet;
 	
+	/**
+	 * Class constructor.
+	 * @param dataSet dataset with cities distances
+	 */
 	public TourCostEvaluator(TSPDataSet dataSet){
 		this.dataSet = dataSet;
 	}
 
+	/**
+	 * Calculate the sum of cities distances of a tour.
+	 */
 	@Override
 	protected double calculateCost(Solution s) {
 		Object[] cities = s.getValues();
@@ -22,5 +35,4 @@ public class TourCostEvaluator extends CostEvaluator {
 		
 		return d;
 	}
-
 }
