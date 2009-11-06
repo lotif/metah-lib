ea03bf228fb5f35ad1399471b59168d5cd600066
paulocmoreno <paulocmoreno@a08dab08-bcae-11de-bb2d-0d8fa6df8a7d>
2009-11-06 15:52:12 +0000

    - Documentada as classes EdgeWeightType e TourCostEvaluator.

--- src/br/unifor/metahlib/problems/tsp/EdgeWeightTypeNotSupported.java
+++ src/br/unifor/metahlib/problems/tsp/EdgeWeightTypeNotSupported.java
@@ -1,7 +1,14 @@
 package br.unifor.metahlib.problems.tsp;
 
+/**
+ * Exception that occurs when the TSPLib file has a not supported EdgeWeightType format. 
+ */
 public class EdgeWeightTypeNotSupported extends Exception {
 	
+	/**
+	 * Exception constructor.
+	 * @param edgeWeightType id of not supported format
+	 */
 	public EdgeWeightTypeNotSupported(String edgeWeightType){
 		super("Edged weight type \"" + edgeWeightType + "\" not supported.");
 	}
