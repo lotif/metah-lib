782570db79335fa4315c1e54702f03cb516edff0
paulocmoreno <paulocmoreno@a08dab08-bcae-11de-bb2d-0d8fa6df8a7d>
2009-11-06 13:43:03 +0000

    - Documentada classe Heuristic.

--- trunk/src/br/unifor/metahlib/base/Heuristic.java
+++ trunk/src/br/unifor/metahlib/base/Heuristic.java
@@ -1,44 +1,54 @@
 package br.unifor.metahlib.base;
 
+/**
+ * This class groups the commons characteristics of heuristics.
+ */
 public abstract class Heuristic {
 
 	/**
-	 * Stores the iteration where the best solution for a function was found.
-	 * Should be set inside the "execute" function of the child class. 
+	 * Stores the iteration where the best solution was found.
+	 * Should be set inside the "execute" method of the child class. 
 	 */
 	protected int lastBestFoundOn = 0;
 	
 	/**
-	 * The problem to be optimized by the heuristic.
+	 * The problem to be optimized by this heuristic.
 	 */
 	protected Problem problem;
 	
+	/**
+	 * Construct a new heuristic for optimize the informed problem.
+	 * @param problem The problem to be optimized.
+	 */
 	public Heuristic(Problem problem){
 		this.problem = problem;
 	}
 	
 	/**
 	 * The method which initiates the optimization.
-	 * @return the result of the optimization
+	 * @return Best solution found during execution.
 	 */
 	public abstract Solution execute();
 	
 	/**
 	 * Retrieves the problem to be optimized by the heuristic.
-	 * @return the problem to be optimized
+	 * @return The problem to be optimized.
 	 */
 	public Problem getProblem(){
 		return problem;
 	}
 	
 	/**
-	 * Set the problem to be optimized by the heuristic
-	 * @param problem The problem to be optimized
+	 * Set the problem to be optimized by the heuristic.
+	 * @param problem The problem to be optimized.
 	 */
 	public void setProblem(Problem problem){
 		this.problem = problem;
 	}
 	
+	/**
+	 * Iteration where the best solution was found.
+	 */
 	public int getLastBestFoundOn() {
 		return lastBestFoundOn;
 	}
