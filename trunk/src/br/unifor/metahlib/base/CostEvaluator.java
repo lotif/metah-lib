760fd6662c43c24a514befac46fc8831ff48d326
paulocmoreno <paulocmoreno@a08dab08-bcae-11de-bb2d-0d8fa6df8a7d>
2009-11-06 13:36:03 +0000

    - Documentada classe CostEvaluator. - Eliminado o conceito de tipo de otimização e padronizado que uma solução de menor custo sempre é melhor. As classes filhas de CostEvalutor devem ser responsáveis por inverter as funções a serem maximizadas.

--- trunk/src/br/unifor/metahlib/base/CostEvaluator.java
+++ trunk/src/br/unifor/metahlib/base/CostEvaluator.java
@@ -1,16 +1,37 @@
 package br.unifor.metahlib.base;
 
+/**
+ * Class used to calculate the cost of the solutions. Lower-cost solutions are always 
+ * best solutions. Therefore, the calculated cost for maximization problems must be inverted.
+ */
 public abstract class CostEvaluator {
 	
+	/**
+	 * Count of calculated solutions.
+	 */
 	private int evalCount = 0;
 	
-	public double eval(Solution s){
+	/**
+	 * Calculates the cost of a solution.
+	 * @param s Solution to evaluate.
+	 * @return Cost of a solution.
+	 */
+	public double eval(Solution s) {
 		evalCount++;
 		return calculateCost(s);
 	}
-	
+
+	/**
+	 * Method responsible for calculate the cost of a solution. Must be implemented 
+	 * in a descending class.
+	 * @param s Solution to evaluate.
+	 * @return Cost of solution.
+	 */
 	protected abstract double calculateCost(Solution s);
 
+	/**
+	 * Count of calculated solutions.
+	 */
 	public int getEvalCount() {
 		return evalCount;
 	}
