7adb76d6c8194130adef09a662b59ab44a5a1aa1
mlotifjava@gmail.com <mlotifjava@gmail.com@a08dab08-bcae-11de-bb2d-0d8fa6df8a7d>
2009-11-13 20:10:22 +0000

    Implementada a metaheurística Variable Neighborhood Search

--- src/br/unifor/metahlib/tests/GLSTest.java
+++ src/br/unifor/metahlib/tests/GLSTest.java
@@ -15,7 +15,7 @@ public class GLSTest {
 	public static void main(String[] args) {
 		try{
 			File file = new File(System.getProperty("user.dir") + "/berlin52.tsp");
-			Object[] optTour = new Object[] {1, 49, 32, 45, 19, 41, 8, 9, 10, 43, 33, 51, 11, 52, 14, 13, 47, 26, 27, 28, 12, 25, 4, 6, 15, 5, 24, 48, 38, 37, 40, 39, 36, 35, 34, 44, 46, 16, 29, 50, 20, 23, 30, 2, 7, 42, 21, 17, 3, 18, 31, 22 };
+//			Object[] optTour = new Object[] {1, 49, 32, 45, 19, 41, 8, 9, 10, 43, 33, 51, 11, 52, 14, 13, 47, 26, 27, 28, 12, 25, 4, 6, 15, 5, 24, 48, 38, 37, 40, 39, 36, 35, 34, 44, 46, 16, 29, 50, 20, 23, 30, 2, 7, 42, 21, 17, 3, 18, 31, 22 };
 			
 //			NeighborhoodStructure neighborhoodStructure = new TwoOpt(); 
 //			TSPProblem problem = new TSPProblem(file, neighborhoodStructure);
@@ -32,7 +32,7 @@ public class GLSTest {
 			 * 3-OPT: 1/10 <= a <= 1/4
 			 * LK-OPT: 1/12 <= a <= 1/6	
 			 */
-			Heuristic gls = new GuidedLocalSearch(problem, h, 2000, 1.0/10.0);
+			Heuristic gls = new GuidedLocalSearch(problem, h, 1000, 1.0/10.0);
 			
 			GreedyConstructiveSolutionGenerator generator = new GreedyConstructiveSolutionGenerator(problem);
 			((GuidedLocalSearch)gls).setInitialSolution(generator.generateNewGreedySolution());
@@ -40,9 +40,9 @@ public class GLSTest {
 			Solution s = gls.execute();
 			System.out.println("Distance: " + s.getCost());
 
-			Solution optimal = new Solution(problem);
-			optimal.setValues(optTour);
-			System.out.println("Optimal: " + optimal);
+//			Solution optimal = new Solution(problem);
+//			optimal.setValues(optTour);
+//			System.out.println("Optimal: " + optimal);
 			
 		} catch (Exception e){
 			e.printStackTrace();
