b39ca9cd28af983e0c09c3665f22657943918e0d
paulocmoreno <paulocmoreno@a08dab08-bcae-11de-bb2d-0d8fa6df8a7d>
2009-12-05 00:14:50 +0000

    - Criado um novo método da estrutura de vizinhança para criar todos os vizinhos de uma solução. - Criado um método para indicar se uma solução inicial foi criada. - Uma solução passa a registrar qual foi a solução da qual se originou. Esta nova propriedade é mantida pelo duplicate(). - Implementada a comparação entre soluções. A menor será a de custo inferior. - Documentação.

--- src/br/unifor/metahlib/base/SolutionFactory.java
+++ src/br/unifor/metahlib/base/SolutionFactory.java
@@ -1,5 +1,13 @@
 package br.unifor.metahlib.base;
 
+/**
+ * A common interface for solution constructors. 
+ */
 public interface SolutionFactory {
+	
+	/**
+	 * Constructs a new solution.
+	 * @return new solution.
+	 */
 	public Solution newSolution();
 }
